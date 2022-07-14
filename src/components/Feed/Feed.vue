30059c76278b57da1c37a92275ebd7e80f14678d
Stef Korporaal <stefkorporaal@gmail.com>
2022-07-14 21:10:30 +0200

    Remove EmptyItem, display Ozymandias quote on empty feed

--- src/components/Feed/Feed.vue
+++ src/components/Feed/Feed.vue
@@ -10,18 +10,6 @@ import { sortStore } from "@s:feed/sort.vue";
 import FeedItemComponent from "@c:Feed/FeedItem.vue";
 import Filter from "@c:Feed/Filter.vue";
 
-const emptyItem = {
-  dot: "bg-gray-500",
-  getFormattedDate() {
-    return "Someday...";
-  },
-  get lang() {
-    return document.documentElement.lang;
-  },
-  title: "There are no posts.",
-  url: "",
-} as unknown as FeedItem;
-
 const { instances } = defineProps<{
   instances: MarkdownInstance<Frontmatter>[];
 }>();
@@ -38,7 +26,7 @@ const filteredItems = computed(() =>
 <template>
   <Filter />
 
-  <ul id="postFeed" class="text-xl md:text-base">
+  <ul v-if="filteredItems.length" id="postFeed" class="text-xl md:text-base">
     <li
       v-for="feedItem in filteredItems"
       :key="feedItem.slug"
@@ -47,4 +35,27 @@ const filteredItems = computed(() =>
       <FeedItemComponent :feedItem="feedItem" />
     </li>
   </ul>
+
+  <div
+    v-else
+    class="my-4 flex flex-col items-center justify-end text-gray-400 dark:text-gray-500 md:mb-0 md:mt-16"
+  >
+    <figure>
+      <blockquote class="mb-4 text-lg italic">
+        <p>Nothing beside remains. Round the decay</p>
+        <p>Of that colossal Wreck, boundless and bare</p>
+        <p>The lone and level sands stretch far away.</p>
+        <cite></cite>
+      </blockquote>
+      <figcaption class="text-xs">
+        <a
+          href="https://www.poetryfoundation.org/poems/46565/ozymandias"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          class="fancy-link italic"
+          >Ozymandias</a
+        >&mdash; Percy Bysshe Shelley
+      </figcaption>
+    </figure>
+  </div>
 </template>
