de6fac6a63e914acb1ff27b8397a1b5e6dfb34a1
Stef Korporaal <stefkorporaal@gmail.com>
2022-07-27 14:21:52 +0200

    Fix deployment

--- deploy.ts
+++ deploy.ts
@@ -1,25 +1,27 @@
-import * as fs from "node:fs/promises";
-import * as path from "node:path";
-import * as ftp from "basic-ftp";
-import * as dotenv from "dotenv";
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import * as ftp from 'basic-ftp';
+import * as dotenv from 'dotenv';
 
 dotenv.config({ override: true });
 
 const PRINTER = {
   depth: 0,
   log(message?: any, ...optionalParams: any[]) {
-    console.log("\t".repeat(this.depth), message, ...optionalParams);
+    console.log('\t'.repeat(this.depth), message, ...optionalParams);
   },
   error(message?: any, ...optionalParams: any[]) {
-    console.error("\t".repeat(this.depth), message, ...optionalParams);
+    console.error('\t'.repeat(this.depth), message, ...optionalParams);
   },
   heading(message?: any, ...optionalParams: any[]) {
     this.log(message, ...optionalParams);
     this.depth++;
   },
-  done() {
-    this.log("Done.");
-    this.depth--;
+  done(decrement: boolean = true) {
+    this.log('Done.');
+    if (decrement) {
+      this.depth--;
+    }
   },
 };
 
@@ -29,7 +31,7 @@ const LOCAL_DIR =
   process.env.LOCAL_DIR && path.join(__dirname, process.env.LOCAL_DIR);
 
 if (!HOSTNAME || !USERNAME || !PASSWORD || !LOCAL_DIR || !REMOTE_DIR) {
-  PRINTER.error("Environment variables not properly configured!");
+  PRINTER.error('Environment variables not properly configured!');
   process.exit(1);
 }
 
@@ -41,7 +43,7 @@ async function checkSitePath(): Promise<void> {
       throw new Error(`'${LOCAL_DIR}' is not a folder.`);
     }
   } catch (error) {
-    if ((error as NodeJS.ErrnoException).code === "ENOENT") {
+    if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
       throw new Error(`There is no '${LOCAL_DIR}'-folder.`);
     } else {
       throw error;
@@ -51,15 +53,15 @@ async function checkSitePath(): Promise<void> {
 
 async function rename404(): Promise<void> {
   PRINTER.log("Renaming '404.html' to '404.shtml'...");
-  const src = path.join(LOCAL_DIR!, "404.html");
-  const tgt = path.join(LOCAL_DIR!, "404.shtml");
+  const src = path.join(LOCAL_DIR!, '404.html');
+  const tgt = path.join(LOCAL_DIR!, '404.shtml');
   await fs.rename(src, tgt);
 }
 
 async function undo404(): Promise<void> {
   PRINTER.log("Renaming '404.shtml' to '404.html'...");
-  const src = path.join(LOCAL_DIR!, "404.shtml");
-  const tgt = path.join(LOCAL_DIR!, "404.html");
+  const src = path.join(LOCAL_DIR!, '404.shtml');
+  const tgt = path.join(LOCAL_DIR!, '404.html');
   await fs.rename(src, tgt);
 }
 
@@ -67,10 +69,12 @@ async function main(): Promise<void> {
   const client = new ftp.Client(60000);
 
   try {
-    PRINTER.heading("Running pre-build tasks:");
+    PRINTER.heading('Running pre-deploy tasks:');
     await rename404();
 
-    PRINTER.log("Attempting to upload files...");
+    PRINTER.done();
+
+    PRINTER.heading('Attempting to upload files...');
     await checkSitePath();
 
     await client.access({
@@ -79,21 +83,21 @@ async function main(): Promise<void> {
       password: PASSWORD,
     });
 
-    PRINTER.log("Connected to FTP...");
+    PRINTER.log('Connected to FTP...');
 
     await client.ensureDir(REMOTE_DIR!);
 
     await client.clearWorkingDir();
-    PRINTER.log("Cleared old files...");
+    PRINTER.log('Cleared old files...');
 
     await client.uploadFromDir(LOCAL_DIR!);
-    PRINTER.log("Uploaded new files...");
+    PRINTER.log('Uploaded new files...');
   } catch (error) {
     console.error((error as Error).message);
   } finally {
     client.close();
     PRINTER.done();
-    PRINTER.heading("Running post-build tasks:");
+    PRINTER.heading('Running post-deploy tasks:');
     await undo404();
 
     PRINTER.done();
