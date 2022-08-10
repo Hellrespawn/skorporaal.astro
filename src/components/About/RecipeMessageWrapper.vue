483e77ce5584fb80c15910d586166cdf3fc3f2d6
Stef Korporaal <stefkorporaal@gmail.com>
2022-08-10 21:35:46 +0200

    Update to Astro 1.0.1, only show message about recipes if recipes are active

---
+++ src/components/About/RecipeMessageWrapper.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { filterStore } from '../../scripts/stores/filter';
+
+const showRecipeMessage = computed(() => filterStore.includes.value('recipe'));
+</script>
+
+<template>
+  <slot />
+  <template v-if="showRecipeMessage">
+    Also, I needed somewhere to store my recipes.</template
+  >
+</template>
