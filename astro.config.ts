36ab4dcfd7e838ab87383f3ec2cf698e34446ed7
Stef Korporaal <stefkorporaal@gmail.com>
2022-09-01 09:56:01 +0200

    Update to Astro 1.1.3
    Use new import system to apply remark and rehype plugins to MD and MDX.

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-
 import mdx from "@astrojs/mdx";
+
+import remarkMath from 'remark-math';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://skorporaal.com',
   markdown: {
     // syntaxHighlight: "prism",
-    remarkPlugins: ['remark-gfm', 'remark-smartypants', 'remark-math'],
-    rehypePlugins: [['rehype-external-links', {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeExternalLinks, {
       rel: ['nofollow', 'noopener', 'noreferrer'],
       target: '_blank'
-    }], 'rehype-katex'],
+    }], rehypeKatex],
+    extendDefaultPlugins: true,
     shikiConfig: {
       wrap: true
     }
