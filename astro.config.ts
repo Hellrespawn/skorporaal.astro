18381fc475a21447cbef9fa34536bbe87c045eea
Stef Korporaal <stefkorporaal@gmail.com>
2022-09-07 12:59:47 +0200

    Update to Astro 1.1.6

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 import remarkMath from 'remark-math';
 import rehypeExternalLinks from 'rehype-external-links';
@@ -11,15 +11,21 @@ export default defineConfig({
   site: 'https://skorporaal.com',
   markdown: {
     // syntaxHighlight: "prism",
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [[rehypeExternalLinks, {
-      rel: ['nofollow', 'noopener', 'noreferrer'],
-      target: '_blank'
-    }], rehypeKatex],
     extendDefaultPlugins: true,
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ['nofollow', 'noopener', 'noreferrer'],
+          target: '_blank',
+        },
+      ],
+      rehypeKatex,
+    ],
     shikiConfig: {
-      wrap: true
-    }
+      wrap: true,
+    },
   },
   integrations: [vue(), mdx()],
 });
--- src/components/ShowIfRecipeSelected.vue
+++ src/components/ShowIfRecipeSelected.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { filterStore } from '../../scripts/stores/filter';
+import { filterStore } from '@s:/stores/filter';
 
 const showRecipeMessage = computed(() => filterStore.includes.value('recipe'));
 </script>
 
 <template>
-  <slot />
   <template v-if="showRecipeMessage">
-    Also, I needed somewhere to store my recipes.</template
-  >
+    <slot />
+  </template>
 </template>
